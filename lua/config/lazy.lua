a9ac5a8e5d555e2d88fa78cda42e3e1f31bfb23f
zachk <zachkhong@trontalgroup.com>
2024-10-25 11:46:25 +0800

    feat: changed default theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "everforest",
+      colorscheme = "base2tone_sea_dark",
     } },
 
     { import = "plugins" },
