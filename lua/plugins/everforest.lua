1f1671ff18b2a1e5ab124b57cdf240e79edd84a8
zachk <zachkhong@trontalgroup.com>
2024-11-12 16:25:29 +0800

    feat: yes

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,24 +1,24 @@
-local M = {
-  "neanias/everforest-nvim",
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-}
-
-function M.config()
-  local everforest = require("everforest")
-  everforest.setup({
-    background = "hard",
-    transparent_background_level = 0,
-    italics = true,
-    disable_italic_comments = false,
-    sign_column_background = "none",
-    ui_contrast = "low",
-    dim_inactive_windows = false,
-    on_highlights = function(hl, _)
-      hl["@string.special.symbol.ruby"] = { link = "@field" }
-    end,
-  })
-  everforest.load()
-end
-
-return M
+-- local M = {
+--   "neanias/everforest-nvim",
+--   lazy = false,
+--   priority = 1000, -- make sure to load this before all the other start plugins
+-- }
+--
+-- function M.config()
+--   local everforest = require("everforest")
+--   everforest.setup({
+--     background = "hard",
+--     transparent_background_level = 0,
+--     italics = true,
+--     disable_italic_comments = false,
+--     sign_column_background = "none",
+--     ui_contrast = "low",
+--     dim_inactive_windows = false,
+--     on_highlights = function(hl, _)
+--       hl["@string.special.symbol.ruby"] = { link = "@field" }
+--     end,
+--   })
+--   everforest.load()
+-- end
+--
+-- return M
