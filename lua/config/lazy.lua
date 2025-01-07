dcdfd5e7ddca18bb757659d8a385a7b0166242ab
zachk <zachkhong@trontalgroup.com>
2025-01-07 19:01:26 +0800

    ghostty is nice

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "base2tone_desert_dark",
+      colorscheme = "base2tone_drawbridge_dark",
     } },
 
     { import = "plugins" },
